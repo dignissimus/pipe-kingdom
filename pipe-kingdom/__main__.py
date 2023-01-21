10672dd36aeaee6d1390b4303ed5d947170d5548
Sam Ezeh <sam.z.ezeh@gmail.com>
2023-01-21 14:39:48 +0000

    Change the background

--- pipe-kingdom/__main__.py
+++ pipe-kingdom/__main__.py
@@ -1,6 +1,7 @@
 import arcade
 from arcade import Window
 
+
 class PipeKingdom(Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
@@ -10,17 +11,27 @@ class PipeKingdom(Window):
 
     def setup(self):
         arcade.set_background_color(arcade.color.AMAZON)
-        self.background = arcade.load_texture("assets/background.webp")
+        self.background = arcade.load_texture("assets/background.jpg")
 
     def on_draw(self):
-        arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
-        arcade.draw_text("Menu", self.width -100, self.height - 100, arcade.color.BLACK, 12)
+        arcade.draw_lrwh_rectangle_textured(
+            0, 0, self.width, self.height, self.background
+        )
+        arcade.draw_text(
+            "Menu",
+            self.width - 400,
+            self.height - 100,
+            arcade.color.BLACK,
+            30,
+            font_name="Kenney Blocks",
+        )
 
 
 def main():
     window = PipeKingdom(1920, 1080, "Pipe Kingdom")
     window.setup()
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
