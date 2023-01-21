e6d5310e63ee8ee561a262e84cf607f09923af36
Sam Ezeh <sam.z.ezeh@gmail.com>
2023-01-21 14:31:21 +0000

    Create base files

--- pipe-kingdom/__main__.py
+++ pipe-kingdom/__main__.py
@@ -1,1 +1,26 @@
 import arcade
+from arcade import Window
+
+class PipeKingdom(Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        self.width = width
+        self.height = height
+        self.title = title
+
+    def setup(self):
+        arcade.set_background_color(arcade.color.AMAZON)
+        self.background = arcade.load_texture("assets/background.webp")
+
+    def on_draw(self):
+        arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
+        arcade.draw_text("Menu", self.width -100, self.height - 100, arcade.color.BLACK, 12)
+
+
+def main():
+    window = PipeKingdom(1920, 1080, "Pipe Kingdom")
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
