d6af786f6d3c32a0a92c60bc7d4aed2695776b9b
Sam Ezeh <sam.z.ezeh@gmail.com>
2023-01-21 16:05:13 +0000

    Add items to the menu

--- pipe-kingdom/__main__.py
+++ pipe-kingdom/__main__.py
@@ -17,11 +17,12 @@ class PipeKingdom(Window):
         self.width = width
         self.height = height
         self.title = title
+        self.show_menu = False
+        self.current_building_type = None
 
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
-
-        self.menu_button = arcade.gui.UIFlatButton(text="Menu", width=200)
+        self.menu_button = arcade.gui.UIFlatButton(text="Show menu", width=200)
         self.manager.add(
             arcade.gui.UIAnchorWidget(
                 anchor_x="right",
@@ -29,10 +30,35 @@ class PipeKingdom(Window):
                 child=self.menu_button,
             )
         )
+        self.menu_button.on_click = self.toggle_menu
+
+        self.menu_box = arcade.gui.UIManager()
+        self.menu_box.enable()
+
+        self.menu_box_layout = arcade.gui.UIBoxLayout(space_between=7.5)
+        self.house_button = arcade.gui.UIFlatButton(text="House", width=400)
+        self.sewage_button = arcade.gui.UIFlatButton(
+            text="Sewage treatment centre", width=400
+        )
+        self.nothing_button = arcade.gui.UIFlatButton(text="Do nothing", width=400)
+        self.menu_box_layout.add(self.house_button)
+        self.menu_box_layout.add(self.sewage_button)
+        self.menu_box_layout.add(self.nothing_button)
+
+        self.menu_box.add(
+            arcade.gui.UIAnchorWidget(
+                anchor_x="center",
+                anchor_y="top",
+                child=self.menu_box_layout,
+            )
+        )
 
         self.sprite_list = SpriteList()
 
         self.buildings = []
+
+    def toggle_menu(self, event):
+        self.show_menu = not self.show_menu
 
     def setup(self):
         arcade.set_background_color(arcade.color.AMAZON)
@@ -45,11 +71,14 @@ class PipeKingdom(Window):
         )
         self.sprite_list.draw()
         self.manager.draw()
+        if self.show_menu:
+            self.menu_box.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        sprite = Sprite("assets/house.png", center_x=x, center_y=y, scale=0.2)
-        self.buildings.append(Building(x, y, sprite))
-        self.sprite_list.append(sprite)
+        if self.current_building_type:
+            sprite = Sprite("assets/house.png", center_x=x, center_y=y, scale=0.1)
+            self.buildings.append(Building(x, y, sprite))
+            self.sprite_list.append(sprite)
 
 
 def main():
