417e9865af29ab807f51b32d3e71c78e6f8f8fcf
Sam Ezeh <sam.z.ezeh@gmail.com>
2023-01-21 20:33:49 +0000

    I'm honestly not sure what this commit does

--- pipe-kingdom/__main__.py
+++ pipe-kingdom/__main__.py
@@ -1,14 +1,19 @@
 import math
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum, auto
+from typing import Tuple
 
 import arcade
 import arcade.gui
 from arcade import Sprite, SpriteList, Window
 
 
-GRID_WIDTH = 30
-GRID_HEIGHT = 40
+GRID_WIDTH = 115
+GRID_HEIGHT = 115
+
+
+# x, y co-ordinates
+Pipe = Tuple[int, int]
 
 
 @dataclass
@@ -16,6 +21,8 @@ class Building:
     x: int
     y: int
     sprite: Sprite
+    pipes: list[Pipe] = field(default_factory=list)
+    buildings: list = field(default_factory=list)
 
     def squared_distance(self, x, y):
         return (self.x - x) ** 2 + (self.y - y) ** 2
@@ -52,7 +59,6 @@ class BuildingType(Enum):
             case BuildingType.WATER_PUMP:
                 return "pump.png"
 
-
     @property
     def is_pipe(self):
         match self:
@@ -62,7 +68,9 @@ class BuildingType(Enum):
 
     @property
     def is_big(self):
-        return self != BuildingType.VERTICAL_PIPE and self != BuildingType.HORIZONTAL_PIPE
+        return (
+            self != BuildingType.VERTICAL_PIPE and self != BuildingType.HORIZONTAL_PIPE
+        )
 
 
 class PipeKingdom(Window):
@@ -107,7 +115,9 @@ class PipeKingdom(Window):
             text="Sewage treatment centre", width=400
         )
         self.nothing_button = arcade.gui.UIFlatButton(text="Do nothing", width=400)
-        self.chemical_plant_button = arcade.gui.UIFlatButton(text="Chemical plant", width=400)
+        self.chemical_plant_button = arcade.gui.UIFlatButton(
+            text="Chemical plant", width=400
+        )
         self.water_pump_button = arcade.gui.UIFlatButton(text="Water pump", width=400)
 
         self.house_button.on_click = self.building_setter(BuildingType.HOUSE)
@@ -123,9 +133,7 @@ class PipeKingdom(Window):
         self.chemical_plant_button.on_click = self.building_setter(
             BuildingType.CHEMICAL_PLANT
         )
-        self.water_pump_button.on_click = self.building_setter(
-            BuildingType.WATER_PUMP
-        )
+        self.water_pump_button.on_click = self.building_setter(BuildingType.WATER_PUMP)
 
         self.nothing_button.on_click = self.building_setter(None)
 
@@ -184,26 +192,38 @@ class PipeKingdom(Window):
             self.menu_box.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if self.current_building_type:
-            if self.current_building_type.is_big:
-                for building in self.buildings:
-                    if building.distance(x, y) < 100:
-                        return
+        if not self.current_building_type:
+            return
 
-            if self.current_building_type.is_pipe:
-                pipe_x = x // GRID_WIDTH
-                pipe_y = (y + 30) // GRID_HEIGHT
-                x = pipe_x * GRID_WIDTH
-                y = pipe_y * GRID_HEIGHT
+        buildings = []
 
-            sprite = Sprite(
-                self.current_building_type.resource, center_x=x, center_y=y, scale=0.1
-            )
-            self.buildings.append(Building(x, y, sprite))
-            self.sprite_list.append(sprite)
+        scale = 0.1
+        if self.current_building_type.is_pipe:
+            pipe_x = x // GRID_WIDTH
+            pipe_y = y // GRID_HEIGHT
+            x = pipe_x * GRID_WIDTH
+            y = pipe_y * GRID_HEIGHT
+            scale = 0.4
 
-            if self.current_building_type.is_pipe:
-                self.pipes[pipe_x][pipe_y] = sprite
+        if self.current_building_type.is_big:
+            for building in self.buildings:
+                if building.distance(x, y) < 100:
+                    return
+        else:
+            # In this case, we're placing a pipe on the map
+            for building in self.buildings:
+                if building.distance(x, y) < 100:
+                    building.pipes.append((pipe_x, pipe_y))
+                    buildings.append(building)
+
+        sprite = Sprite(
+            self.current_building_type.resource, center_x=x, center_y=y, scale=scale
+        )
+        self.buildings.append(Building(x, y, sprite, buildings=buildings))
+        self.sprite_list.append(sprite)
+
+        if self.current_building_type.is_pipe:
+            self.pipes[pipe_x][pipe_y] = sprite
 
 
 def main():
